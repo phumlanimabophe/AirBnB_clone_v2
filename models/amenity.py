e24a508733b01e987c4a5099f27dfbec14789b23
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2023-09-20 19:58:08 +0000

    p/m

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
+from models.base import Base
+
+association_table = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True,
+                                 nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True,
+                                 nullable=False))
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
-    __tablename__ = "amenities"
+    """Amenity class"""
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    place_amenities = relationship("Place",
+                                   secondary=association_table,
+                                   back_populates="amenities")
--- models/base_model.py
+++ models/base_model.py
@@ -1,71 +1,27 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base import Base
 
-from datetime import datetime
-import models
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-import uuid
+class Place(BaseModel, Base):
+    """Place class"""
+    __tablename__ = 'places'
+    # ... (other attributes)
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
+    # Add the association with Amenity
+    amenities = relationship("Amenity",
+                            secondary=association_table,
+                            viewonly=False,
+                            back_populates="place_amenities")
 
-if models.storage_t == "db":
-    Base = declarative_base()
-else:
-    Base = object
+    @property
+    def amenities(self):
+        """Getter attribute for amenities"""
+        return self.amenities
 
-
-class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
-    if models.storage_t == "db":
-        id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.utcnow)
-        updated_at = Column(DateTime, default=datetime.utcnow)
-
-    def __init__(self, *args, **kwargs):
-        """Instantiate a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            storage.new(self)
-        else:
-            for k, v in kwargs.items():
-                if k != '__class__':
-                    setattr(self, k, v)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.utcnow()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.utcnow()
-
-    def __str__(self):
-        """Return a string representation of the instance"""
-        cls_name = self.__class__.__name__
-        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
-
-    def save(self):
-        """Update updated_at with the current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.utcnow()
-        storage.new(self)
-        storage.save()
-
-    def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = dict(self.__dict__)
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        dictionary.pop('_sa_instance_state', None)
-        return dictionary
-
-    def delete(self):
-        """Delete the current instance from the storage (models.storage)"""
-        from models import storage
-        storage.delete(self)
-        storage.save()
+    @amenities.setter
+    def amenities(self, amenity):
+        """Setter attribute for amenities"""
+        if isinstance(amenity, Amenity):
+            self.amenities.append(amenity)
